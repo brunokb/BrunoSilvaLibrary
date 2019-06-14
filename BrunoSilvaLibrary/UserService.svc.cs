8250f5919546a648e5bced3369fdeb9f6640de40
Bruno Silva <bruno.k.silva@gmail.com>
2019-06-14 15:34:54 +1000

    wcf

---
+++ BrunoSilvaLibrary/UserService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BrunoSilvaLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
+    public class UserService : IUserService
+    {
+        public void DoWork()
+        {
+        }
+        public int userReturn()
+        {
+            return 1;
+        }
+
+    }
+}
